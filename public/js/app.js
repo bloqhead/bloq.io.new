bb3748f5d1b4b7e42d87e04b19182860dad5d8d5
Daryn St. Pierre <daryn@bigseadesign.com>
2017-05-22 10:10:10 -0400

    More page element design.

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,48 @@
-"use strict";
+'use strict';
 
 (function () {
 
-	/**
-  * Testing/debugging
-  */
-	var thing = "thing";
-	console.log(thing);
+	//
+	// Flying Triangles
+	//
 
-	/**
-  * Hamburger
-  */
+	var colors = ['#D70E2F', '#F54768', '#8BD76B', '#07EFF3', '#EEF46E'];
 
-	/**
-  * Logo Animation
-  */
+	// triangle animations
+	var triangles = anime({
+		targets: '.shape svg',
+		rotateZ: function rotateZ() {
+			return anime.random(20, 1000);
+		},
+		rotateX: function rotateX() {
+			return anime.random(20, 1000);
+		},
+		rotateY: function rotateY() {
+			return anime.random(20, 1000);
+		},
+		translateZ: function translateZ() {
+			return anime.random(0, 1000);
+		},
+		scale: function scale() {
+			return anime.random(1, 5.5);
+		},
+		easing: 'easeOutElastic',
+		elasticity: 300,
+		duration: 10000,
+		direction: 'alternate',
+		loop: true
+	});
+
+	// triangle polygon animations
+	var polygons = anime({
+		targets: '.shape svg g',
+		stroke: function stroke() {
+			return colors[Math.floor(Math.random() * colors.length)];
+		},
+		easing: 'easeInOutCirc',
+		duration: 5000,
+		direction: 'alternate',
+		loop: true
+	});
 })();
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,34 @@
 (function() {
 
-	/**
-	 * Testing/debugging
-	 */
-	const thing = "thing";
-	console.log(thing);
+//
+// Flying Triangles
+//
 
-	/**
-	 * Hamburger
-	 */
+const colors = [ '#D70E2F', '#F54768', '#8BD76B', '#07EFF3', '#EEF46E' ];
 
+// triangle animations
+const triangles = anime({
+	targets: '.shape svg',
+	rotateZ: () => ( anime.random( 20, 1000 ) ),
+	rotateX: () => ( anime.random( 20, 1000 ) ),
+	rotateY: () => ( anime.random( 20, 1000 ) ),
+	translateZ: () => ( anime.random( 0, 1000 ) ),
+	 scale: () => (anime.random(1, 5.5)),
+	easing: 'easeOutElastic',
+	elasticity: 300,
+	duration: 10000,
+	direction: 'alternate',
+	loop: true
+});
 
-	/**
-	 * Logo Animation
-	 */
+// triangle polygon animations
+const polygons = anime({
+	targets: '.shape svg g',
+	stroke: () => colors[Math.floor(Math.random()*colors.length)],
+	easing: 'easeInOutCirc',
+	duration: 5000,
+	direction: 'alternate',
+	loop: true
+});
 
 })();
